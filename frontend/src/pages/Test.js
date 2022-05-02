c65e5b7cf23f202ee9beb270c270538435ca3b58
Rdspinks7 <104697184+Rdspinks7@users.noreply.github.com>
2022-05-02 15:50:06 -0500

    Update Test.js

--- frontend/src/pages/Test.js
+++ frontend/src/pages/Test.js
@@ -18,12 +18,12 @@ const TEST = () => {
 
   return (
     <>
-      <div class="sidebar">
-        <div class="logo-details">
-          <span class="user_name">name </span>
-          <span class="user_name2">position</span>
-        </div>
-      </div>
+       <div class="sidebar">
+    <div class="logo-details">
+       <span class="user_name">name </span>
+       <span class="user_name2">position</span>
+       </i>
+  </div>
       <ul class="nav-links">
         <li>
           <a href="#" class= "active">
@@ -32,66 +32,72 @@ const TEST = () => {
           </a>
         </li>
         <li>
-          <a href="#" class= "active2">
-            <i class='bx bx-box' />
-            <span class="links_name">Messages</span>
-          </a>
-        </li>
+          <ul class="nav-links2">
+            <li>
+              <a href="#" class= "active2">
+                <i class='bx bx-boxx' ></i>
+                 <span class="links_name">Messages</span>
+             </a>
+          </li>
         <li>
-          <a href="#" class= "active3">
-            <i class='bx bx-heart' ></i>
-            <span class="links_name">Settings</span>
+          <ul class="nav-links3">
+            <li>
+              <a href="#" class= "active3">
+                <i class='bx bx-heart' ></i>
+               <span class="links_name">Settings</span>
+              </a>
+             </li>
+       <li>
+     <div class="buttons3">
+        <div class="action_btn3">      
+          <button name="enter" class="action_btn3 log out" type="submit" value="Log Out" onclick="Interaction">Log out</button>
           </a>
         </li>
       </ul>
-      <div class="buttons3">
-        <div class="action_btn3">      
-          <button name="enter" class="action_btn3 log out" type="submit" value="Log Out" onclick="Interaction">Log out</button>
-        </div>
+  </div>
+   <section class="home-section">
+    <nav>
+      <div class="search-box">
+        <input type="text" placeholder="Search Dashboard...">
       </div>
-      <section class="home-section">
-        <nav>
-          <div class="search-box">
-            <input type="text" placeholder="Search Dashboard..."/>
-          </div>
-          <div class = "dashboard">Dashboard</div>
-          <div class="profile-details"> 
-          </div>
-        </nav>
-        <div class="home-content">
-          <div class="overview-boxes">
-            <div class="box">
-              <div class="right-side">
-                <div class="box-topic">Clients</div>
-                <div class="number">2500</div>
-              </div>
+      <div class = "dashboard">Dashboard</div>
+      
+      </div>
+    </nav>
+    <div class="home-content">
+      <div class="overview-boxes">
+        <div class="box">
+          <div class="right-side">
+            <div class="box-topic">Clients</div>
+            <div class="number">2500</div>
             </div>
-            <i class='bx bx-cart-alt cart'></i>
           </div>
-          <div class="sales-boxes">
-            <div class="recent-activity box"></div>
-          </div>
-          <div class="title">Overview</div>
-            <div>
-              <div class= "container">Recent Activity</div>
-              <span class="contacts"></span>
-              <div class ="rosa"> Rosa</div>
-              <div class ="ashley">Ashley</div>
-              <img src="static/Ashley.jpeg"/>
-              <span class="contacts2"></span>
-              <div class ="bryan">Bryan</div>
-              <img src="static/Bryan.jpeg"/>
-              <span class="contacts3"></span>
-              <div class ="sarah">Sarah</div>
-              <img src="static/Sarah.jpeg"/>
-              <span class ="contacts4"></span> 
-            </div>
-            <div class="buttons">
-              <div class="action_btn">
-                <button name="enter" class="action_btn1 view clients" type="submit" value="View Clients" onclick="Interaction">View Clients</button>
-                <button name="enter" class="action_btn2 access logs" type="submit" value="Access Logs" onclick="Interaction">Access Logs</button>
-                <p id="saved"></p>
-              </div>
+          <i class='bx bx-cart-alt cart'></i>
+      </div>
+      <div class="sales-boxes">
+        <div class="recent-activity box">
+    <div class="title">Overview</div>
+          </ul>
+         <div>
+           <div class= "container">Recent Activity</div>
+           <span class ="contacts"</span>
+           <div class ="rosa"> Rosa</div>
+            <div class ="ashley">Ashley</div>
+               <img src="Ashley.jpeg">
+             <span class ="contacts2"</span>
+            <div class ="bryan">Bryan</div>
+               <img src="Bryan.jpeg">
+             <span class ="contacts3"</span>
+            <div class ="sarah">Sarah</div>
+               <img src="Sarah.jpeg">
+             <span class ="contacts4"</span> 
+        </div>
+        <div class="buttons">
+          <div class="action_btn">
+            <button name="enter" class="action_btn1 view clients" type="submit" value="View Clients" onclick="Interaction">View Clients</button>
+            <button name="enter" class="action_btn2 access logs" type="submit" value="Access Logs" onclick="Interaction">Access Logs</button>
+ <p id="saved"></p>
+
             </div>
           </div>
         </section>
