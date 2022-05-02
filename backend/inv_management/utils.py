6b8a27c0b606c0cecaf4ee6fd366230abf5e80d0
Nic <nico.nguyen22@gmail.com>
2022-05-01 19:23:19 -0500

    added db cursor accessible from utils file

--- backend/inv_management/utils.py
+++ backend/inv_management/utils.py
@@ -1,6 +1,13 @@
+from os import getenv
 from json import loads
+from MySQLdb import connect
+from dotenv import load_dotenv
 
 # extracts the body of a post request
 def getBody(request):
   body_unicode = request.body.decode('utf-8')
   return loads(body_unicode)
+
+load_dotenv()
+dbconnect = connect("localhost", getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_NAME'))
+cursor = dbconnect.cursor()
